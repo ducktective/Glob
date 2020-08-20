7a11452e544d1e9336e70ee241af08638219337a
Albert <sleappn@gmail.com>
2020-08-20 17:37:08 +0300

    ex_7 download data

---
+++ airflow/ex7/test_dag_download_data.py
@@ -0,0 +1,54 @@
+'''
+    This is test dag for ex7.
+    It download data from excel file to vertica DB.
+'''
+from test_download_data.test_download_data import DownloadData
+from airflow import DAG
+from airflow.operators.python_operator import PythonOperator
+from datetime import datetime, timedelta
+from airflow.models import Variable
+# this line for connection
+from airflow.models.connection import Connection
+
+# setting for DAG
+default_args = {
+    'owner': 'airflow',
+    'depends_on_past': False,
+    'start_date': datetime(2020, 8, 10),
+}
+
+# i haven't schedual for this ex.
+dag = DAG(
+    dag_id='test_ex_7',
+    default_args=default_args,
+    schedule_interval=None)
+
+var = Variable.get('test_Variables')
+
+
+def check_var_task():
+    DownloadData.check_var(var)
+
+
+def check_column_task():
+    DownloadData.check_column(var)
+
+
+def download_to_vertica_task():
+    DownloadData.download_to_vertica(var)
+
+
+with dag:
+    task1_download_data = PythonOperator(
+        task_id='task_download_data',
+        python_callable=check_var_task)
+
+    task2_check_column = PythonOperator(
+        task_id='task_check_column',
+        python_callable=check_column_task)
+
+    task3_download_to_vertica = PythonOperator(
+        task_id='task_download_to_vertica',
+        python_callable=download_to_vertica_task)
+
+    task1_download_data >> task2_check_column >> task3_download_to_vertica
---
+++ airflow/ex7/test_download_data.py
@@ -0,0 +1,85 @@
+'''
+This script downloading data in to vertica.
+'''
+
+import vertica_python
+import numpy as np
+import pandas as pd
+import xlrd
+import re
+import os
+from airflow.exceptions import AirflowSkipException
+from airflow.exceptions import AirflowException
+import logging
+
+# next line for connection to vertica
+from airflow.hooks.base_hook import BaseHook
+import shutil
+
+
+class DownloadData():
+
+    @staticmethod
+    def check_var(var_value):
+        '''
+            var_value is 
+            Check for directory and file on existing.
+        '''
+        dir_name = re.match(r'.*/', var_value).group(0)
+        if os.path.isdir(dir_name) == False:
+            logging.warning('This directory doesn\'t exist')
+            raise AirflowSkipException()
+        elif os.path.isfile(var_value) == False:
+            logging.warning('This file doesn\'t exist')
+            raise AirflowSkipException()
+
+    @staticmethod
+    def check_column(file_):
+        '''
+        Take file in file_ variable.
+        '''
+        df = pd.read_excel(file_)
+        column_tuple = ('Commodity', 'Cause', 'Region',
+                        'Country / Territory', 'Company',
+                        'Site', 'Plant No.', 'Estimated Start Date',
+                        'Expected Annual Capacity Change (kt)',
+                        'Total Annual Capacity (kt)', 'Swing Capable')
+        for column in column_tuple:
+            if column not in df.columns:
+                logging.error(f'Dataframe {file_} didn\'t pass validation.')
+                del df
+                raise AirflowException()
+                return False
+        logging.info(f'Dataframe {file_} pass validation.')
+        del df
+        return True
+
+    @staticmethod
+    def convert_to_csv(file_):
+        '''
+        Take excel file whith data. Then transform in on csv format and download this in vertica.
+        '''
+        df = pd.read_excel(file_)
+        dir_name = re.match(r'.*/', file_).group(0)
+        new_csv_file = os.path.join(dir_name, 'NEW_CAPACITIES.csv')
+        df.to_csv(new_csv_file, index=False)
+        del df
+        return new_csv_file
+
+    @staticmethod
+    def download_to_vertica(file_):
+        csv_file = DownloadData.convert_to_csv(file_)
+        connection = BaseHook.get_connection(
+            'Test_bad_Vertica_connection')
+        connection_info = {'host': connection.host,
+                           'port': 5433,
+                           'user':  connection.login,
+                           'password':  connection.password,
+                           'database':  connection.schema}
+        with vertica_python.connect(**connection_info) as conn:
+            cur = conn.cursor()
+            cur.execute(
+                fr"COPY test_vert.STG_NEW_CAPACITIES(commodity,cause,region,country_or_teritory,company,site,plant_no,estimated_start_date,exp_ann_cap_change,total_annual, swing_capable) FROM local '{csv_file}' PARSER fcsvparser() ABORT ON ERROR;")
+            loaded_data = os.path.join(
+                '/usr', 'local', 'airflow', 'data', 'test_loaded_data')
+            shutil.copyfile(csv_file, loaded_data)
